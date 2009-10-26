5c3e805871e32ca3f9ffd16b065d5af37d96a7fe
sonic <sonic@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-26 11:15:17 +0000

    CHG: raised version number to 0.0.3dev
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_mvc/trunk@25816 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "pt_mvc"
 #
-# Auto generated 18-09-2008 10:40
+# Auto generated 26-10-2009 12:13
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.2dev',
+	'version' => '0.0.3dev',
 	'constraints' => array(
 		'depends' => array(
 			'pt_tools' => '0.4.2-',
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 			'If tx_ptmvc_controllerCli is used: PEAR Console_Color (THIS IS JUST A HINT, please ignore if your server is correctly configured)' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"0548";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"26d7";s:20:"doc/wizard_form.html";s:4:"c204";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"d0eb";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"5837";s:17:"ext_localconf.php";s:4:"0a5e";s:14:"doc/DevDoc.txt";s:4:"02db";s:14:"doc/manual.sxw";s:4:"2a3c";s:37:"classes/class.tx_ptmvc_controller.php";s:4:"1321";s:44:"classes/class.tx_ptmvc_controllerBackend.php";s:4:"8790";s:40:"classes/class.tx_ptmvc_controllerCli.php";s:4:"8b58";s:40:"classes/class.tx_ptmvc_controllerEid.php";s:4:"8f0d";s:45:"classes/class.tx_ptmvc_controllerFrontend.php";s:4:"cc9d";s:35:"classes/class.tx_ptmvc_dbObject.php";s:4:"5f18";s:45:"classes/class.tx_ptmvc_dbObjectCollection.php";s:4:"f4d7";s:45:"classes/class.tx_ptmvc_dbObjectRepository.php";s:4:"ca9b";s:30:"classes/class.tx_ptmvc_div.php";s:4:"f03e";s:31:"classes/class.tx_ptmvc_view.php";s:4:"bb85";}',
+	'suggests' => array(
+	),
 );
 
 ?>
